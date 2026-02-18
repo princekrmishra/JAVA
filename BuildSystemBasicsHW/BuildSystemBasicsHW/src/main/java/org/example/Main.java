e8114163314d61106149a2814a630647bb769df1
Prince Kumar Mishra <kmishraprince@gmail.com>
2026-02-18 14:06:17 +0530

    Added retrofti support with GSON

--- BuildSystemBasicsHW/BuildSystemBasicsHW/src/main/java/org/example/Main.java
+++ BuildSystemBasicsHW/BuildSystemBasicsHW/src/main/java/org/example/Main.java
@@ -1,11 +1,22 @@
 package org.example;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-       
+    public static void main(String[] args) throws IOException {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        TodoService todoService = retrofit.create(TodoService.class);
+        Todo t = todoService.getTodo("3").execute().body();
+
+        System.out.println("Todo object downloaded is: " + t.toString());
     }
 }
